126226027dad98d0142d3ee2e60f31d229f7e2e1
MaiaPiko <maia.mlynczak@gmail.com>
2023-10-20 05:04:59 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import RandomBlob from './components/RandomBlob'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 function App() {
   
   return (
